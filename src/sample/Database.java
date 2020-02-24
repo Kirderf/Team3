1d3662ba5db816ca7f560e6226516b559f70b62d
Fredrik Holm Julsen <fredrjul@stud.ntnu.no>
2020-02-24 14:31:04 +0100

    Added database methods

--- src/sample/Database.java
+++ src/sample/Database.java
@@ -1,14 +1,36 @@
 package sample;
+import java.sql.*;
+import java.util.Random;
+
+//TODO: add javadoc
 
 public class Database {
-    public void init(){
+    Random random = new Random();
+    String table = "fredrjul_"+random.nextInt(10000000);
 
+    /**
+     * This method is going to create a new database table and declare a variable to the rest of the class
+     *
+     */
+    public void init() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver"); // Java database driver for mySql
+            // Url to database, username and password to setup connection to database
+            Connection con=DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/fredrjul_ImageApp","fredrjul_Image","Password123");
+            Statement stmt=con.createStatement();
+            stmt.executeQuery("CREATE TABLE "+ table);
+            con.close();
+        }catch(Exception e){
+            System.out.println("Something is wrong with database setup");
+            e.printStackTrace();}
     }
 
     /** This method is going to write data to the database
-     * @param
+     * //TODO: Add parameter to method
+     * @param  // data to add
      */
     public void writeToDatabase(){
+
 
     }
 
@@ -16,7 +38,9 @@ public class Database {
      *
      */
     public void readDatabase(){
-
+    //TODO: Add method functionality
     }
-
+    private boolean isTableInDatabase(String databaseName){
+        return false;
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception{
+        new Database().init();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 900   ,900 ));
