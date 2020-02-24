db9a62446adeac038394e070ccca69b83bbcb068
Fredrik Holm Julsen <fredrjul@stud.ntnu.no>
2020-02-24 16:11:47 +0100

    Added java connector to pom.xml. Added regTable method to database. Created Databasetest class

--- Tests/sample/DatabaseTest.java
+++ Tests/sample/DatabaseTest.java
@@ -1,18 +1,31 @@
 package sample;
 
+import backend.Database;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.sql.SQLOutput;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseTest {
-
+    Database database;
     @BeforeEach
     void setUp() {
+        database = new Database();
     }
 
     @Test
     void init() {
+        try {
+            database.openConnection();
+            database.regTable();
+            database.openConnection();
+        } catch (Exception e) {
+            System.out.println("Error setting ut database");
+            e.printStackTrace();
+        }
     }
 
     @Test
--- src/backend/Database.java
+++ src/backend/Database.java
@@ -6,41 +6,59 @@ import java.util.Random;
 
 public class Database {
     Random random = new Random();
-    String table = "fredrjul_"+random.nextInt(10000000);
+    String table = "fredrjul_" + random.nextInt(10000000);
+    Connection con;
+
+    public void init() {
+        while (isTableInDatabase(table)) {
+
+        }
+    }
 
     /**
      * This method is going to create a new database table and declare a variable to the rest of the class
-     *
      */
-    public void init() {
-        try{
-            Class.forName("com.mysql.jdbc.Driver"); // Java database driver for mySql
-            // Url to database, username and password to setup connection to database
-            Connection con=DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/fredrjul_ImageApp","fredrjul_Image","Password123");
-            Statement stmt=con.createStatement();
-            stmt.executeQuery("CREATE TABLE "+ table);
-            con.close();
-        }catch(Exception e){
-            System.out.println("Something is wrong with database setup");
-            e.printStackTrace();}
+    public void regTable() throws Exception {
+        // Url to database, username and password to setup connection to database
+
+        PreparedStatement stmt = con.prepareStatement(
+                "CREATE TABLE " + table + " (\n" +
+                "    ImageID int NOT NULL AUTO_INCREMENT,\n" +
+                "    Path varchar(255),\n" +
+                "    Tags varchar(255),\n" +
+                "    PRIMARY KEY (ImageID)\n" +
+                ");");
+        stmt.execute();
     }
 
-    /** This method is going to write data to the database
+    /**
+     * This method is going to write data to the database
      * //TODO: Add parameter to method
-     * @param  // data to add
+     *
+     * @param // data to add
      */
-    public void writeToDatabase(){
-
+    public void writeToDatabase() throws SQLException {
+    PreparedStatement preparedStatement = con.prepareStatement(
+            "SELECT "+ table +" "
+    );
 
     }
 
     /**
      *
      */
-    public void readDatabase(){
-    //TODO: Add method functionality
+    public void readDatabase() {
+        //TODO: Add method functionality
     }
-    private boolean isTableInDatabase(String databaseName){
+
+    private boolean isTableInDatabase(String tableName) {
         return false;
+    }
+
+    public void openConnection() throws SQLException {
+        con = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no:3306/fredrjul_ImageApp", "fredrjul_Image", "Password123");
+    }
+    public void closeConnection() throws SQLException {
+        con.close();
     }
 }
