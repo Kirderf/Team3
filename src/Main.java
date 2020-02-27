f7b5a57b6a99ece12d94beaaf0b5c8f396b4eee5
erlin <erlinssl@stud.ntnu.no>
2020-02-27 12:24:30 +0100

    Created sample2.fxml, a new suggestion to the main window. Uses a borderpane as a base, and has splitpanes for more user customizability.

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Views/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Views/sample2.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800   ,500 ));
         primaryStage.show();
