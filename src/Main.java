1c4ddf0da58b3d2849ca215ab284fcaa4d50892d
Erling Sung Sletta <erlinssl@stud.ntnu.no>
2020-02-29 19:23:55 +0100

    Reintroduced insertImage in importAction for testing purposes

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("Views/Main.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/Views/Main.fxml"));
             primaryStage.setTitle("Hello World");
             primaryStage.setScene(new Scene(root, 800, 600));
             primaryStage.show();
