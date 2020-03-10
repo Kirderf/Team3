e6acd35d420809abc00e31d429cddb66ed48b10b
Tommy Luu <tommydl@stud.ntnu.no>
2020-03-11 00:25:40 +0100

    Welcome message

--- src/backend/Text_To_Speech.java
+++ src/backend/Text_To_Speech.java
@@ -8,6 +8,10 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 import java.io.IOException;
 import java.util.logging.Level;
 
+/**
+ * @author goxr3plus, Tommy Luu
+ * Text-to-speech using google translate API
+ */
 public class Text_To_Speech {
     //Create a Synthesizer instance
     SynthesiserV2 synthesizer = new SynthesiserV2("AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw");
@@ -15,30 +19,24 @@ public class Text_To_Speech {
 
     public Text_To_Speech() throws IOException {
         //Startup message
-        speak(GoogleTranslate.translate("ja","Hello how are you?"));
+        speak("お前はもう死んでいるのですが、ご主人様お元気ですか。");
     }
 
     /**
      * Calls the MaryTTS to say the given text
-     *
      * @param text
      */
     public void speak(String text) {
         System.out.println(text);
-
         //Create a new Thread because JLayer is running on the current Thread and will make the application to lag
         Thread thread = new Thread(() -> {
             try {
                 //Create a JLayer instance
                 AdvancedPlayer player = new AdvancedPlayer(synthesizer.getMP3Data(text));
                 player.play();
-
-                logger.log(Level.WARNING, "Successfully received synthesizer data");
-
+                logger.log(Level.INFO, "Successfully received synthesizer data");
             } catch (IOException | JavaLayerException e) {
-
                 e.printStackTrace(); //Print the exception ( we want to know , not hide below our finger , like many developers do...)
-
             }
         });
 
