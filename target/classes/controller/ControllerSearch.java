b9b81eec72be6260bcb4cbb51328496cbec47991
ikhovind <ikhovind@stud.ntnu.no>
2020-03-16 09:11:31 +0100

    search by filename finished

--- target/classes/controller/ControllerSearch.java
+++ target/classes/controller/ControllerSearch.java
@@ -33,7 +33,7 @@ public class ControllerSearch implements Initializable {
     @FXML
     CheckBox pathCheck;
     @FXML
-    CheckBox nameCheck;
+    CheckBox filenameCheck;
     @FXML
     TextField searchField;
     @FXML
@@ -67,20 +67,6 @@ public class ControllerSearch implements Initializable {
 
        insertTags();    //  for ScrollSearch
 
-
-/*              Old tag search, uses the searchField
-        if(tagCheck.isSelected()){
-            ArrayList<String> tagResult = ControllerMain.databaseClient.search(searchField.getText(),"Tags");
-            if (tagResult!=null) {
-                for(String s : tagResult){
-                    if(!searchResults.contains(s)){
-                        searchResults.add(s);
-                    }
-                }
-            }
-        }
-
- */
         if(metaCheck.isSelected()){
             ArrayList<String> metaResult = ControllerMain.databaseClient.search(searchField.getText(),"Metadata");
             if (metaResult!=null) {
@@ -101,6 +87,21 @@ public class ControllerSearch implements Initializable {
                 }
             }
         }
+
+        if(filenameCheck.isSelected()){
+            //finds the matching paths
+            ArrayList<String> filenameResult = ControllerMain.databaseClient.search(searchField.getText(),"Path");
+            if(filenameResult!= null) {
+                for (String s : filenameResult) {
+                    //specifies that we only want the ones where the actual filename contains the search term
+                    if (s.substring(s.lastIndexOf("/")).contains(searchField.getText())) {
+                        searchResults.add(s);
+                    }
+                }
+            }
+        }
+
+
         searchSucceed = true;
         cancel(event);
     }
