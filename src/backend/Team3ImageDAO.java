62b71f064473c9fb0660fb15281013148c71ddf9
ikhovind <ikhovind@stud.ntnu.no>
2020-03-24 14:32:17 +0100

    minor bug fix, still does not work

--- src/backend/Team3ImageDAO.java
+++ src/backend/Team3ImageDAO.java
@@ -105,11 +105,12 @@ public class Team3ImageDAO {
         Team3ImageDAO facade = null;
         System.out.println("start...");
         try{
+
             emf = Persistence.createEntityManagerFactory("LecturePU");
             //LecturePU=Persistence Unit Name, see persistence.xml
             System.out.println("EntityManagerFactory created " + emf);
             facade = new Team3ImageDAO(emf);
-            System.out.println("BookDAO created");
+            System.out.println("Team3ImageDAO created");
             //create an Image using the set methods
             Team3Image team3Image = new Team3Image();
             team3Image.setPath("Hjem til jul");
@@ -125,7 +126,7 @@ public class Team3ImageDAO {
             team3Image = new Team3Image("mamam",55,new Date(19991103),22,55,535.2,663.3);
             facade.storeNewTeam3Image(team3Image);
 //List books
-            System.out.println("These books are stored in the database:");
+            System.out.println("These Images are stored in the database:");
             List<Team3Image> imageList = facade.getAllTeam3Images();
             for (Team3Image t : imageList){
                 System.out.println("---" + t);
@@ -134,14 +135,14 @@ public class Team3ImageDAO {
             team3Image.setPath("changed path");
             facade.editTeam3Image(team3Image);
             team3Image = facade.findTeam3Image(team3Image.getPath());//fetch book
-            System.out.println("Book changed and is now like this: " + team3Image);
+            System.out.println("Image changed and is now like this: " + team3Image);
 //find number of books in database
-            int numberOfBooks = facade.getNumberOfTeam3Images();
-            System.out.println("There are " + numberOfBooks + " books in the database");
+            int numberOfTeam3Images = facade.getNumberOfTeam3Images();
+            System.out.println("There are " + numberOfTeam3Images + " images in the databse in the database");
 //list books by a chosen author
             String theAuthor = "Author2";
             imageList = facade.getTeam3ImageWithGPSData();
-            System.out.println(" These books exists made by " + theAuthor + " " + imageList.size());
+            System.out.println(" These images exists with valid gps data" + imageList.size());
             for (Team3Image b : imageList){
                 System.out.println("\t" + b.getLatitude() + "," + b.getLongitude());
             }
