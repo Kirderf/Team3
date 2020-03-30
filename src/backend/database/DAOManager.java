6af50a76d909cab60bae68424387f7fad03a7f25
Fredrik Holm Julsen <Fredrjul@stud.ntnu.no>
2020-03-30 17:34:27 +0200

    Testing uniqueContstraints

--- src/backend/database/DAOManager.java
+++ src/backend/database/DAOManager.java
@@ -1,72 +1,65 @@
 package backend.database;
 
 import javax.naming.InitialContext;
-import javax.persistence.EntityManagerFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
+
 //TODO add logger to entire class
 //https://stackoverflow.com/questions/12812256/how-do-i-implement-a-dao-manager-using-jdbc-and-connection-pools
 public class DAOManager {
-    private DataSource src = new DataSource();
     private Connection con;
 
-    public Connection getCon() throws SQLException{
+    DAOManager(){
+
+    }
+
+    public static DAOManager getInstance() {
+        return DAOManagerSingleton.INSTANCE.get();
+    }
+
+    public Connection getCon() {
         try {
             open();
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return con;
     }
-    DAOManager() throws Exception{
-        try {
-            InitialContext ctx = new InitialContext();
-        } catch (Exception e){
-            //TODO implement logger
-            e.printStackTrace();
-        }
-    }
-    public static DAOManager getInstance(){
-        return DAOManagerSingleton.INSTANCE.get();
-    }
-    public void open() throws SQLException {
+
+    public void open() {
         try {
             if (this.con == null || this.con.isClosed()) {
-                this.con = src.getConnection();
+                this.con = DataSource.getConnection();
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void close() throws SQLException{
+
+    public void close() {
         try {
-            if(this.con != null && !this.con.isClosed()){
+            if (this.con != null && !this.con.isClosed()) {
                 this.con.close();
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private static class DAOManagerSingleton {
         static final ThreadLocal<DAOManager> INSTANCE;
-        static
-        {
+
+        static {
             ThreadLocal<DAOManager> dm;
-            try
-            {
+            try {
                 dm = ThreadLocal.withInitial(() -> {
-                    try
-                    {
+                    try {
                         return new DAOManager();
-                    }
-                    catch(Exception e)
-                    {
+                    } catch (Exception e) {
                         return null;
                     }
                 });
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
                 dm = null;
             }
--- src/backend/database/Team3Image.java
+++ src/backend/database/Team3Image.java
@@ -1,18 +1,19 @@
 package backend.database;
 
-import org.eclipse.persistence.annotations.Customizer;
 import org.eclipse.persistence.descriptors.ClassDescriptor;
 
-import javax.naming.Name;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.io.Serializable;
-import java.sql.Connection;
-import java.util.Random;
 import javax.persistence.*;
+import java.io.Serializable;
+
 //should we have a named query?
 //named this way to avoid confusion with existing Image classes
 @Entity
+@Table(
+        name = "TEAM3IMAGE",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "path")
+        }
+)
 public class Team3Image implements Serializable {
     @Id
     private String path;
@@ -25,7 +26,6 @@ public class Team3Image implements Serializable {
     private String tags;
 
     public Team3Image(String path, int fileSize, long date, int imageHeight, int imageWidth, double latitude, double longitude) {
-        String.valueOf(new Random().nextInt(500));
         this.path = path;
         this.fileSize = fileSize;
         this.date = date;
@@ -35,7 +35,8 @@ public class Team3Image implements Serializable {
         this.longitude = longitude;
         this.tags = "";
     }
-    public Team3Image(){
+
+    public Team3Image() {
         ClassDescriptor.shouldUseFullChangeSetsForNewObjects = true;
     }
 
