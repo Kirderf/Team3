9a098ea5792ed698e31e47401fab8544cf035d23
Ingebrigt Kristoffer Thomassen Hovind <ikhovind@stud.ntnu.no>
2020-04-10 11:54:17 +0200

    logger now saves three log files

--- src/backend/util/Log.java
+++ src/backend/util/Log.java
@@ -1,6 +1,9 @@
 package backend.util;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
@@ -9,19 +12,19 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class Log{
-    public Logger logger = Logger.getAnonymousLogger();
+    public static Logger logger = Logger.getAnonymousLogger();
     FileHandler fh;
-
+    private static ArrayList<String> logs = new ArrayList<>();
+    private static String currentLog = pickLog();
     public Log(){
-        String fileName = "Log.log";
-        //deletes the old log file every time
-        new File(fileName).delete();
+        //the oldest of the three log files
+        new File(currentLog).delete();
         try{
-            File f = new File(fileName);
+            File f = new File(currentLog);
             f.createNewFile();
 
             LogManager.getLogManager().reset();
-            fh=new FileHandler(fileName,true);
+            fh=new FileHandler(currentLog,true);
             fh.setLevel(Level.FINE);
             logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
             logger.addHandler(fh);
@@ -30,6 +33,22 @@ public class Log{
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+    public static String pickLog(){
+        //three valid logs
+        logs.add("Log1.log");
+        logs.add("Log2.log");
+        logs.add("Log3.log");
+        //places the one that is the oldest last
+        logs.sort((o1, o2) -> {
+            File file1 = new File(o1);
+            File file2 = new File(o2);
+            //oldest last, so need to sort descending
+            return (int) -(file1.lastModified() - file2.lastModified());
+        });
+        //chooses the last log, aka the oldest one
+        currentLog = logs.get(logs.size()-1);
+        return currentLog;
     }
 
     public void logNewInfo(String message){
